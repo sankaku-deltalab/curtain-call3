1c38c0ed88cfbfa4e0211883c2b18097fad8e273
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-11-10 21:03:57 +0900

    Add overlap calculation

---
+++ src/core/components/collision/overlap-calculation.ts
@@ -0,0 +1,113 @@
+import boxIntersect = require('box-intersect');
+import {pipe} from 'rambda';
+import {Enum, RecM2M, RecM2MTrait, RecSet, RecSetTrait} from '../../utils';
+import {Collision, CollisionMode, CollisionShape} from './collision';
+
+export type CollisionKey = string;
+
+type Box2d = [number, number, number, number]; // [minX, minY, maxX, maxY]
+
+type CollisionForCalc = {
+  key: CollisionKey;
+  aabb: Box2d;
+  shape: CollisionShape;
+  mode: CollisionMode;
+  excess: boolean;
+};
+
+export class OverlapCalculation {
+  static calcOverlaps(collisions: Record<CollisionKey, Collision>): RecM2M {
+    const r = pipe(
+      () => collisions,
+      getCollisionsForCalc,
+      splitCollisionsToExcessAndNonExcess,
+      calcAabbOverlaps,
+      overlaps => filterOverlapsByMode(overlaps, collisions)
+    )();
+
+    return r;
+  }
+}
+
+const getCollisionsForCalc = (
+  collisions: Record<CollisionKey, Collision>
+): CollisionForCalc[] => {
+  return Object.entries(collisions).flatMap(([key, col]) => {
+    return col.shapes.map(s => {
+      const aabb: Box2d = [s.box.nw.x, s.box.nw.y, s.box.se.x, s.box.se.y];
+      return {...col, key, shape: s, aabb};
+    });
+  });
+};
+
+const splitCollisionsToExcessAndNonExcess = (
+  collisions: CollisionForCalc[]
+): {excess: CollisionForCalc[]; nonExcess: CollisionForCalc[]} => {
+  const excess = collisions.filter(c => c.excess);
+  const nonExcess = collisions.filter(c => !c.excess);
+  return {excess, nonExcess};
+};
+
+const calcAabbOverlaps = (collisions: {
+  excess: CollisionForCalc[];
+  nonExcess: CollisionForCalc[];
+}): RecM2M => {
+  const eCol = collisions.excess;
+  const neCol = collisions.nonExcess;
+
+  const eAabb = Enum.map(eCol, c => c.aabb);
+  const neAabb = Enum.map(neCol, c => c.aabb);
+
+  const eVsNe = boxIntersect(eAabb, neAabb);
+  const eVsE = boxIntersect(eAabb);
+
+  const eVsNeKeys: [string, string][] = Enum.map(eVsNe, ([i, j]) => [
+    eCol[i].key,
+    neCol[j].key,
+  ]);
+  const eVsEKeys: [string, string][] = Enum.map(eVsE, ([i, j]) => [
+    eCol[i].key,
+    eCol[j].key,
+  ]);
+
+  const overlapKeys = [...eVsNeKeys, ...eVsEKeys];
+  const overlapKeysRev: [string, string][] = Enum.map(
+    overlapKeys,
+    ([k1, k2]) => [k2, k1]
+  );
+  const overlapKeysAll = [...overlapKeys, ...overlapKeysRev];
+
+  return RecM2MTrait.fromPairs(overlapKeysAll);
+};
+
+const filterOverlapsByMode = (
+  aabbOverlaps: RecM2M,
+  collisions: Record<CollisionKey, Collision>
+): RecM2M => {
+  const filterOpponents = (
+    self: CollisionKey,
+    opponents: RecSet
+  ): [CollisionKey, RecSet] => {
+    const selfCol = collisions[self];
+    const filteredOpponents = RecSetTrait.filter(opponents, c => {
+      const opponentCol = collisions[c];
+      return canOverlap(selfCol, opponentCol);
+    });
+    return [self, filteredOpponents];
+  };
+
+  return pipe(
+    () => aabbOverlaps,
+    overlapsSet => Object.entries(overlapsSet),
+    overlapsPairs =>
+      Enum.map(overlapsPairs, ([self, opponents]) => [
+        self,
+        filterOpponents(self, opponents),
+      ]),
+    overlapsPairs => Object.fromEntries(overlapsPairs)
+  )();
+};
+
+const canOverlap = (col_a: Collision, col_b: Collision): boolean => {
+  return (col_a.mode.mode & col_b.mode.mask) > 0;
+};
--- src/core/utils/util.ts
+++ src/core/utils/util.ts
@@ -120,6 +120,19 @@ export class Im {
   }
 }
 
+export class Enum {
+  static map<T, T2>(iter: Iterable<T>, converter: (v: T) => T2): T2[] {
+    return [...iter].map(converter);
+  }
+
+  static filter<T, T2 extends T>(
+    iter: Iterable<T>,
+    filter: (v: T) => v is T2
+  ): T2[] {
+    return [...iter].filter(filter);
+  }
+}
+
 export type RecSet = Record<string, true | undefined>;
 
 export class RecSetTrait {
@@ -142,5 +155,57 @@ export class RecSetTrait {
 
   static iter(set: RecSet): string[] {
     return Object.keys(set).filter(key => RecSetTrait.has(set, key));
+  }
+
+  static filter(set: RecSet, filter: (key: string) => boolean): RecSet {
+    const items = this.iter(set).filter(filter);
+    return RecSetTrait.new(items);
+  }
+}
+
+/**
+ * Record represents many to many relation.
+ * If it has A to B relation, rel is {A: {B: true, ...}, ...}.
+ */
+export type RecM2M = Record<string, RecSet>;
+
+export class RecM2MTrait {
+  static new(): RecM2M {
+    return {};
+  }
+
+  static addRelation(rel: RecM2M, from: string, to: string): RecM2M {
+    if (from in rel) {
+      return Im.replace(rel, from, opponents => RecSetTrait.add(opponents, to));
+    }
+
+    return Im.add(rel, from, RecSetTrait.new([to]));
+  }
+
+  static hasRelation(rel: RecM2M, from: string, to: string): boolean {
+    const opponents: RecSet | undefined = rel[from];
+    if (opponents === undefined) {
+      return false;
+    }
+
+    return to in opponents;
+  }
+
+  static fromPairs(pairs: [string, string][]): RecM2M {
+    return pairs.reduce(
+      (rel, curr) => RecM2MTrait.addRelation(rel, curr[0], curr[1]),
+      RecM2MTrait.new()
+    );
+  }
+
+  static toPairs(rel: RecM2M): [string, string][] {
+    return Object.entries(rel).flatMap(([from, opponents]) => {
+      return RecSetTrait.iter(opponents).map<[string, string]>(o => [from, o]);
+    });
+  }
+
+  static merge(a: RecM2M, b: RecM2M): RecM2M {
+    const pairs = [...this.toPairs(a), ...this.toPairs(b)];
+    return this.fromPairs(pairs);
   }
 }
