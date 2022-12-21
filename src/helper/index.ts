2dda04cb0f652cfd64340ddd05f66a10fa37d36e
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-12-21 10:50:48 +0000

    Fix index.ts

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,5 +1,6 @@
 export * from './actress-helper';
 export * from './collision-helper';
+export * from './game-processing-helper';
 export * from './game-state-helper';
 export * from './graphic-helper';
 export * from './input-helper';
