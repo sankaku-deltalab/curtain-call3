84ee80251574e79eec1dc9360b99c8e1e0f61959
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-01-30 13:25:45 +0000

    Fix index.ts

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -2,6 +2,7 @@ export * from './actress-parts';
 export * from './camera';
 export * from './collision';
 export * from './cue';
+export * from './data-sources';
 export * from './graphics';
 export * from './inputs';
 export * from './time';
