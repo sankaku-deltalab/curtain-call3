e8e60faeef334ed726343a98a2bff20ce09c70ff
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-03-22 13:32:57 +0000

    Add OverlapsReducerProcedure

--- src/core/user-defined-processors/procedure.ts
+++ src/core/user-defined-processors/procedure.ts
@@ -1,6 +1,12 @@
 import {GameState} from '../state/game-states';
-import {Body, BodyType, DataDefinition} from '../setting/data-definition';
+import {
+  Body,
+  BodyId,
+  BodyType,
+  DataDefinition,
+} from '../setting/data-definition';
 import {TBodiesState} from '../state/state-components/bodies-state';
+import {TCollisionState} from '../state/state-components/collision-state';
 
 export interface Procedure<Def extends DataDefinition> {
   apply(state: GameState<Def>): GameState<Def>;
@@ -22,5 +28,34 @@ export abstract class BodiesReducerProcedure<
     let bodies = TBodiesState.getBodiesInType(state.bodies, this.type);
     bodies = bodies.filter(b => this.filterBody(b, state));
     return bodies.reduce((state, b) => this.applyBody(state, b), state);
+  }
+}
+
+export abstract class OverlapsReducerProcedure<
+  Def extends DataDefinition,
+  LeftBT extends BodyType<Def>,
+  RightBT extends BodyType<Def>
+> implements Procedure<Def>
+{
+  abstract readonly leftBodyType: LeftBT;
+  abstract readonly rightBodyType: RightBT;
+
+  abstract applyOverlaps(
+    state: GameState<Def>,
+    leftBodyId: BodyId<Def, LeftBT>,
+    rightBodyId: BodyId<Def, RightBT>
+  ): GameState<Def>;
+
+  apply(state: GameState<Def>): GameState<Def> {
+    const overlapPairs = TCollisionState.getOverlapsInType(
+      state.collision,
+      this.leftBodyType,
+      this.rightBodyType
+    );
+
+    return overlapPairs.reduce(
+      (state, [b1, b2]) => this.applyOverlaps(state, b1, b2),
+      state
+    );
   }
 }
