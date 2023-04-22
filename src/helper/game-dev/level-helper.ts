cfe234e37b4f1471b4617ddfdc00c533393ca7fc
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-04-22 10:15:23 +0000

    Add LevelHelper.updateLevel

--- src/helper/game-dev/level-helper.ts
+++ src/helper/game-dev/level-helper.ts
@@ -1,6 +1,7 @@
 import {DataDefinition, Level} from '../../core/setting/data-definition';
 import {GameState} from '../../core/state/game-states';
 import {TLevelState} from '../../core/state/state-components/level-state';
+import {Im} from '../../utils';
 
 export class LevelHelper {
   static getLevel<Def extends DataDefinition>(
@@ -17,5 +18,17 @@ export class LevelHelper {
       ...state,
       level: TLevelState.putLevel(state.level, level),
     };
+  }
+
+  static updateLevel<Def extends DataDefinition>(
+    state: GameState<Def>,
+    updater: (level: Level<Def>) => Level<Def>
+  ): GameState<Def> {
+    const newLevel = Im.pipe(
+      () => state,
+      state => this.getLevel(state),
+      lv => updater(lv)
+    )();
+    return this.putLevel(state, newLevel);
   }
 }
